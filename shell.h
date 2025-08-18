c0f257b26eb245ec4f55439354253457fcbe50be
Thea33710 <thea33710@gmail.com>
2025-08-18 10:13:57 +0200

    .h

---
+++ shell.h
@@ -0,0 +1,19 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#define MAX_INPUT 1024
+#define MAX_ARGS 64
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+extern char **environ;
+
+void execute_command(char **args);
+void print_env(void);
+int parse_input(char *line, char **args);
+
+#endif
