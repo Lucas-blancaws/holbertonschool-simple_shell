cc1eb8142201fb7664953ed7614fd3ab2ec4ed59
Lucas-blancaws <lblancportier@gmail.com>
2025-08-19 15:17:58 +0200

    Add main shell loop and input handling

--- simple_shell.c
+++ simple_shell.c
@@ -10,6 +10,7 @@ int main(void)
 	char *line = NULL;
 	size_t len = 0;
 	char *args[MAX_ARGS];
+	char *cmd;
 
 	while (1)
 	{
@@ -20,7 +21,6 @@ int main(void)
 			printf("\n");
 			break;
 		}
-
 		line[strcspn(line, "\n")] = '\0';
 
 		if (strcmp(line, "exit") == 0)
@@ -33,9 +33,16 @@ int main(void)
 			print_env();
 			continue;
 		}
-
 		if (parse_input(line, args))
-			execute_command(args);
+		{
+			cmd = find_command(args[0]);
+			if (!cmd)
+			{
+				fprintf(stderr, "%s: command not found\n", args[0]);
+				continue;
+			}
+			execute_command(args, cmd);
+		}
 	}
 
 	free(line);
