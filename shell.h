93d7d9f739791f0e79b035c6936518f9eae1b381
Thea33710 <thea33710@gmail.com>
2025-08-19 11:25:35 +0200

    shell

--- shell.h
+++ shell.h
@@ -1,7 +1,6 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#define MAX_INPUT 1024
 #define MAX_ARGS 64
 
 #include <stdio.h>
@@ -15,5 +14,7 @@ extern char **environ;
 void execute_command(char **args);
 void print_env(void);
 int parse_input(char *line, char **args);
+char *find_command(char *cmd);
+char *get_path(void);
 
 #endif
--- simple_shell.c
+++ simple_shell.c
@@ -1,95 +1,21 @@
 #include "shell.h"
 
 /**
- * print_env - prints the current environment
- */
-
-void print_env(void)
-{
-	int i;
-
-	for (i = 0; environ[i] != NULL; i++)
-	{
-		printf("%s\n", environ[i]);
-	}
-}
-
-/**
- * parse_input - splits a line into arguments
- * @line: input line
- * @args: array of arguments
- * Return: 1 if args found, 0 otherwise
- */
-
-int parse_input(char *line, char **args)
-{
-	int i;
-	char *token;
-
-	i = 0;
-	token = strtok(line, " ");
-	while (token != NULL && i < MAX_ARGS - 1)
-	{
-		args[i++] = token;
-		token = strtok(NULL, " ");
-	}
-	args[i] = NULL;
-
-	if (args[0] == NULL)
-		return (0);
-
-	return (1);
-}
-
-/**
- * execute_command - executes a command or builtin
- * @args: array of arguments
- */
-
-void execute_command(char **args)
-{
-	pid_t pid;
-	int status;
-
-	if (strcmp(args[0], "env") == 0)
-	{
-		print_env();
-		return;
-	}
-
-	pid = fork();
-	if (pid == 0)
-	{
-		execvp(args[0], args);
-		perror("execvp");
-		exit(EXIT_FAILURE);
-	}
-	else if (pid > 0)
-	{
-		waitpid(pid, &status, 0);
-	}
-	else
-	{
-		perror("fork");
-	}
-}
-
-/**
  * main - entry point of simple shell
  * Return: 0 on success
  */
 
 int main(void)
 {
-	char line[MAX_INPUT];
+	char *line = NULL;
+	size_t len = 0;
 	char *args[MAX_ARGS];
 
 	while (1)
 	{
 		printf("$ ");
-		fflush(stdout);
 
-		if (fgets(line, sizeof(line), stdin) == NULL)
+		if (getline(&line, &len, stdin) == -1)
 		{
 			printf("\n");
 			break;
@@ -100,9 +26,16 @@ int main(void)
 		if (strcmp(line, "exit") == 0)
 			break;
 
+		if (strcmp(line, "env") == 0)
+		{
+			print_env();
+			continue;
+		}
+
 		if (parse_input(line, args))
 			execute_command(args);
 	}
 
+	free(line);
 	return (0);
 }
