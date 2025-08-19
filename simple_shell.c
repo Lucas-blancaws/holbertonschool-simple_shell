938cc9faccbee5c3cbf8640e17cb55880d7c57d8
Lucas-blancaws <lblancportier@gmail.com>
2025-08-19 11:42:14 +0200

    Implement core shell logic

---
+++ simple_shell.c
@@ -0,0 +1,41 @@
+#include "shell.h"
+
+/**
+ * main - entry point of simple shell
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	char *args[MAX_ARGS];
+
+	while (1)
+	{
+		printf("$ ");
+
+		if (getline(&line, &len, stdin) == -1)
+		{
+			printf("\n");
+			break;
+		}
+
+		line[strcspn(line, "\n")] = '\0';
+
+		if (strcmp(line, "exit") == 0)
+			break;
+
+		if (strcmp(line, "env") == 0)
+		{
+			print_env();
+			continue;
+		}
+
+		if (parse_input(line, args))
+			execute_command(args);
+	}
+
+	free(line);
+	return (0);
+}
